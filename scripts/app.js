016c4e642b9670a89ddb126104a0accd166916e9
Lissandre <lissandre.pasdeloup@hetic.net>
2018-09-25 17:19:34 +0200

    Begin Arrow Animation

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,16 @@
-let menu = document.querySelector('.hamburger')
+//Menu hamburger
+const menu = document.querySelector('.hamburger')
 menu.addEventListener('click', ()=>{
   menu.classList.toggle('is-active')
 })
+
+//Arrow animation
+let upArrowPosition = upArrow.getBoundingClientRect()
+let downArrowPosition = downArrow.getBoundingClientRect()
+const upArrow = document.querySelector('.upArrow'), downArrow = document.querySelector('.downArrow'), direction = -1
+function arrowAnimation(){
+  if () {
+
+  }
+}
+intervalID = window.setInterval(arrowAnimation, 100)
